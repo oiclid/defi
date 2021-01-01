4f977648c8c5044f9bded9fbbdb949ff489dd64e
oiclid <iff@oiclid.com>
2021-01-01 08:55:31 +0100

    fixed path

--- contracts/liquiditypool/LiquidityPool.sol
+++ contracts/liquiditypool/LiquidityPool.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/access/Ownable.sol";
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/ReentrancyGuard.sol";
 import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/utils/Address.sol";
